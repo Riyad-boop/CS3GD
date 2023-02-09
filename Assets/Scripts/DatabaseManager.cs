53a54eb45b1be69a3dbd0df198b2ccbf63168719
Riyad <180129650@aston.ac.uk>
2023-02-09 19:40:45 +0000

    backendless score storage

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using Firebase.Database;
 using TMPro;
+using System.Collections;
+using System;
 
 public class DatabaseManager : MonoBehaviour
 {
     public TMP_InputField usernameInput;
+    public TMP_Text username_text;
+    public TMP_Text score_text;
     public float score;
     private string user_id;
     private DatabaseReference dbReference;
@@ -18,11 +22,43 @@ public class DatabaseManager : MonoBehaviour
         dbReference = FirebaseDatabase.DefaultInstance.RootReference;
     }
 
-    public void CreateUser() {
+    /// <summary>
+    /// Write's user's data to the firebase database
+    /// </summary>
+    public void WriteUser() {
         User user = new User(usernameInput.text, score);
-        Debug.Log(dbReference.Reference);
         string json = JsonUtility.ToJson(user);
         dbReference.Child("users").Child(user_id).SetRawJsonValueAsync(json);
+    }
+
+    /// <summary>
+    /// asynchronously fetches the user data and creates a user object from it 
+    /// </summary>
+    /// <param name="callback"> contains the user's data </param>
+    /// <returns></returns>
+    public IEnumerator GetUser(Action<User> callback)
+    {
+        var dbTask = dbReference.Child("users").Child(user_id).GetValueAsync();
+        yield return new WaitUntil(predicate:() => dbTask.IsCompleted);
+
+        if (dbTask != null)
+        {
+            DataSnapshot snapshot = dbTask.Result;
+            User dbUser = new User(snapshot.Child("username").Value.ToString(), float.Parse(snapshot.Child("score").Value.ToString()));
+            callback.Invoke(dbUser);
+        }
+    }
+
+    /// <summary>
+    /// Starts an asynchronous function to fetch the user's data
+    /// </summary>
+    public void ReadUser()
+    {
+        StartCoroutine(GetUser((User user) =>
+        {
+            username_text.text = user.username;
+            score_text.text = user.score.ToString();
+        }));
     }
   
 }
